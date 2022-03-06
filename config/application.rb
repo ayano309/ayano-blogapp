33d6fa939aa59198a75840d111c9b17ab59407aa
綾乃 <>
2022-03-06 18:52:58 +0900

    編集ボタンを作成

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module AyanoBlogapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.generators do |g|  # ここから追記
+      g.assets false          # CSS, JavaScriptファイル生成せず
+      g.skip_routes false     # trueならroutes.rb変更せず、falseなら通常通り変更
+      g.test_framework false  # testファイル生成せず
+    end                       # ここまで
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
