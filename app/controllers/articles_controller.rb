412634d95eaeb99ca5c6b538a124d0ed318d059e
綾乃 <>
2022-03-07 16:20:57 +0900

    before_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only:[:show,:edit,:update]
+
+
     def index
         
         @articles =Article.all
@@ -24,10 +27,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+        
     end
     def update
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             redirect_to article_path(@article), notice: '更新しました'
         else
@@ -46,4 +49,9 @@ class ArticlesController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :content)
     end
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
 end
