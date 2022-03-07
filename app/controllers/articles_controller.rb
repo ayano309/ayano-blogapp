e2e6f0442144953cdeb305759214aef913f083d1
綾乃 <>
2022-03-07 14:42:08 +0900

    editページの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,12 @@ class ArticlesController < ApplicationController
             render :new
         end
     end
+
+    def edit
+        @aricle = Article.find(params[:id])
+    end
+
+
     private
     def article_params
         params.require(:article).permit(:title, :content)
