e6eea98d77f589ef5f7cd9533f722e031797e87f
綾乃 <>
2022-03-08 23:51:44 +0900

    has_many, belongs_to を記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,11 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
     validates :title, presence: true
@@ -19,12 +24,14 @@ class Article < ApplicationRecord
 
     validate :validate_title_and_content_length
 
+    belongs_to :user
+
     def display_created_at
         I18n.l(self.created_at, format: :default)
     end
     private
     def validate_title_and_content_length
-      char_count = self.title.length + self.content.length
-      errors.add(:content, '100文字いじょうで！') unless char_count > 100
+    char_count = self.title.length + self.content.length
+    errors.add(:content, '100文字いじょうで！') unless char_count > 100
     end
 end
