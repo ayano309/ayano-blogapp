326219d8c0c808061f8033016946fda91edcaf1d
綾乃 <>
2022-03-18 00:12:24 +0900

    Decoratorで書き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,16 +35,13 @@ class User < ApplicationRecord
   has_one :profile,dependent: :destroy
 
   delegate :birthday, :age,:gender, to: :profile, allow_nil: true
+
   def has_written?(article)
     articles.exists?(id: article.id)
-  end
+    end
 
   def has_liked?(article)
     likes.exists?(article_id: article.id)
-  end
-
-  def display_name
-    profile&.nickname || self.email.split('@').first
   end
 
   def follow!(user)
@@ -64,18 +61,11 @@ class User < ApplicationRecord
   end
 
 
-
   def prepare_profile
     profile || build_profile
   end
 
-  def avatar_image
-    if profile&.avatar&.attached?
-      profile.avatar
-    else
-      'default-avatar.png'
-    end
-  end
+
   private
   def get_user_id(user)
     if user.is_a?(User)
