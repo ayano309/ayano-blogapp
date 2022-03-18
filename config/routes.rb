d3b2ca22abf35577dcbc190f303f5a0a107579ee
綾乃 <>
2022-03-18 15:41:59 +0900

    serializer を使って comments の json を作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
 
   resources :articles do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: [:index, :new, :create]
 
     resource :like, only: [:show, :create, :destroy]
   end
