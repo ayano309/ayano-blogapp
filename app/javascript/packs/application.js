3fa3f7d6ac0464b26642c96d43ee61293185661a
綾乃 <>
2022-03-18 15:11:09 +0900

    いいねの状態をjQueryで反映

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -40,7 +40,10 @@ document.addEventListener('turbolinks:load', () => {
     $('.inactive-heart').on('click', () => {
         axios.post(`/articles/${articleId}/like`)
             .then((response) => {
-                console.log(response)
+                if (response.data.status === 'ok') {
+                    $('.active-heart').removeClass('hidden')
+                    $('.inactive-heart').addClass('hidden')
+                }
             })
             .catch((e) => {
                 window.alert('Error')
@@ -51,7 +54,10 @@ document.addEventListener('turbolinks:load', () => {
         $('.active-heart').on('click', () => {
             axios.delete(`/articles/${articleId}/like`)
             .then((response) => {
-                console.log(response)
+                if (response.data.status === 'ok') {
+                    $('.active-heart').addClass('hidden')
+                    $('.inactive-heart').removeClass('hidden')
+                }
             })
             .catch((e) => {
                 window.alert('Error')
